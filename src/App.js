ab5322c1602c7116451d8f95134f98d20cb75908
Sam Chillcott <samchillcott@gmail.com>
2020-03-21 13:05:23 +0000

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>   
-    </div>
-  );
+import './App.css';
+
+class App extends Component {
+  render() {    
+      return (
+          <div className="App">
+              <h1>App</h1>   
+          </div>
+      );
+  }    
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Todos extends Component {
+    render() {    
+        return (
+            <div>
+                <h1>Todos</h1>   
+            </div>
+        );
+    }    
+}
+
+export default Todos;
