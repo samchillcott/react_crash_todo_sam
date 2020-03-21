64dd9ac045908422a6183260e7136056f29a8b34
Sam Chillcott <samchillcott@gmail.com>
2020-03-21 15:17:55 +0000

    test

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
   render() {    
       return (
           <div className="App">
-              <Todos />   
+              <Todos todos={this.state.todos} />   
           </div>
       );
   }    
