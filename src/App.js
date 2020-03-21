c61b96fc076bb87e76060ae5e5ceaf9b5c9868bb
Sam Chillcott <samchillcott@gmail.com>
2020-03-21 13:21:20 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with Wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with Boss',
+        completed: false
+      },
+    ]
+  }
   render() {    
       return (
           <div className="App">
