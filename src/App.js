22a4f7eeb461bcc74eab71f05710ce1d09caef9c
Sam Chillcott <samchillcott@gmail.com>
2020-03-22 16:45:53 +0000

    22/3/20 commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
+import * as uuid from 'uuid';
 
 import './App.css';
 
@@ -7,27 +12,66 @@ class App extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with Wife',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with Boss',
         completed: false
       },
     ]
   }
+
+// Toggle Complete
+
+markComplete = (id) => {
+  this.setState({ todos: this.state.todos.map(todo => {
+    if(todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo;
+  })})
+}
+  
+// Delete Todo
+delTodo = (id) => {
+  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+}
+
+// Add Todo
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(),
+    title,
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo] })
+}
+
   render() {    
       return (
+        <Router>
           <div className="App">
-              <Todos todos={this.state.todos} />   
+            <div className="container">  
+              <Header />
+              <Route exact path="/" render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />   
+                </React.Fragment>
+              )} />
+              <Route path="/about" component={About} />
+              
+            </div>
           </div>
+        </Router>    
       );
   }    
 }
