877c69984acb8ba790db1205808cd6a0ba5eb0cd
Sam Chillcott <samchillcott@gmail.com>
2020-05-06 09:35:37 +0100

    prettier

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class AddTodo extends Component {
-    state = {
-        title: ''
-    }
+	state = {
+		title: "",
+	};
 
-    onSubmit = (e) => {
-        e.preventDefault();
-        this.props.addTodo(this.state.title);
-        this.setState({ title: '' });
-    }
-    
-    onChange = (e) => this.setState({ title: e.target.value });
-    
-    render() {
-        return (
-            <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
-                <input 
-                    type="text" 
-                    name="title" 
-                    style={{ flex: '10', padding: '5px' }}
-                    placeholder="Add Todo..." 
-                    value={this.state.title}
-                    onChange={this.onChange}
-                />
-                <input
-                    type="submit"
-                    value="submit"
-                    className="btn"
-                    style={{flex: '1'}}
-                />
-            </form>
-        )
-    }
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.props.addTodo(this.state.title);
+		this.setState({ title: "" });
+	};
+
+	onChange = (e) => this.setState({ title: e.target.value });
+
+	render() {
+		return (
+			<form onSubmit={this.onSubmit} style={{ display: "flex" }}>
+				<input
+					type="text"
+					name="title"
+					style={{ flex: "10", padding: "5px" }}
+					placeholder="Add Todo..."
+					value={this.state.title}
+					onChange={this.onChange}
+				/>
+				<input
+					type="submit"
+					value="submit"
+					className="btn"
+					style={{ flex: "1" }}
+				/>
+			</form>
+		);
+	}
 }
 
 AddTodo.propTypes = {
-    AddTodo: PropTypes.func.isRequired,
-}
+	AddTodo: PropTypes.func.isRequired,
+};
 
-export default AddTodo
+export default AddTodo;
